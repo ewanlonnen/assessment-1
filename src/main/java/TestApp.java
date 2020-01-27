62a6e4eecbe7635b39ae3f2ea0edcd8dbca9329f
Ewan Lonnen <40410790@live.napier.ac.uk>
2020-01-27 17:21:09 +0000

    MongoDB with self-contained JAR

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -1,6 +1,7 @@
 public class TestApp {
     public static void main(String[] args)
     {
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         System.out.println("Testing!");
         System.out.println("Testing new branches");
         System.out.println("Adding new print for develop branch");
