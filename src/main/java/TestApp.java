824ede16f6868e7ab41a3f51018eaa9a3d5cf056
Ewan Lonnen <40410790@live.napier.ac.uk>
2020-02-03 13:43:26 +0000

    Adding Docker compile and update travis file

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -1,12 +1,57 @@
+import java.sql.*;
 public class TestApp {
-    public static void main(String[] args)
     {
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        System.out.println("Testing!");
-        System.out.println("Testing new branches");
-        System.out.println("Adding new print for develop branch");
-        System.out.println("Testing commits...");
-        System.out.println("Testing commits...");
-        System.out.println("Testing commits...");
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        // Connection to the database
+        Connection con = null;
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                // Wait a bit
+                Thread.sleep(10000);
+                // Exit for loop
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
     }
 }
