d06dbe7ccbc633ffafd8901a1f15a0ae69c3655c
CameronStott <40416068@live.napier.ac.uk>
2020-02-24 13:17:32 +0000

    creating limit

--- src/main/java/group21/assessment/Country.java
+++ src/main/java/group21/assessment/Country.java
@@ -1,5 +1,7 @@
 package group21.assessment;
 
+import java.util.ArrayList;
+
 public class Country {
 
     public String code;
@@ -21,8 +23,17 @@ public class Country {
         region = Region;
         population = Population;
         capital = Capital;
+    }
+    Country(){
 
+    }
 
+    public void generateReport(ArrayList<Country> regionPop){
+        for (Country country : regionPop) {
+            String country_string =
+                    String.format("%-5s %-30s %-20s %-20s %-15s %-15s", country.code, country.name, country.continent, country.region, country.population, country.capital);
+            System.out.println(country_string);
+        }
     }
 
 }
--- src/main/java/group21/assessment/TestApp.java
+++ src/main/java/group21/assessment/TestApp.java
@@ -68,13 +68,6 @@ public class TestApp {
             //ArrayList<Country> countryList = new ArrayList<Country>();
             while (rset.next()) {
                 Country c = new Country(rset.getString("country.Code"), rset.getString("country.Name"), rset.getString("country.Continent"),rset.getString("country.Region"),rset.getInt("country.Population"),rset.getString("city.Name"));
-                /*c.code = rset.getString("country.Code");
-                c.name = rset.getString("country.Name");
-                c.continent = rset.getString("country.Continent");
-                c.region = rset.getString("country.Region");
-                c.population = rset.getInt("country.Population");
-                c.capital = rset.getString("city.Name");*/
-
                 return c;
             }
             return null;
@@ -99,12 +92,6 @@ public class TestApp {
             ArrayList<Country> countryList = new ArrayList<Country>();
             while (rset.next()) {
                 Country c = new Country(rset.getString("country.Code"), rset.getString("country.Name"), rset.getString("country.Continent"),rset.getString("country.Region"),rset.getInt("country.Population"),rset.getString("city.Name"));
-                /*c.code = rset.getString("country.Code");
-                c.name = rset.getString("country.Name");
-                c.continent = rset.getString("country.Continent");
-                c.region = rset.getString("country.Region");
-                c.population = rset.getInt("country.Population");
-                c.capital = rset.getString("city.Name");*/
                 countryList.add(c);
             }
             return countryList;
@@ -121,7 +108,7 @@ public class TestApp {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
-            String strSelect = "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.name  FROM country, city WHERE country.Capital = city.ID and country.Continent = " + "'" + s_continent + "'" + " ORDER BY Population DESC";
+            String strSelect = "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.name FROM world.country left join world.city on country.Capital = city.ID WHERE country.Continent = " + "'" + s_continent + "'" + " order by country.Population DESC";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -129,12 +116,53 @@ public class TestApp {
             ArrayList<Country> countryList = new ArrayList<Country>();
             while (rset.next()) {
                 Country c = new Country(rset.getString("country.Code"), rset.getString("country.Name"), rset.getString("country.Continent"),rset.getString("country.Region"),rset.getInt("country.Population"),rset.getString("city.Name"));
-                /*c.code = rset.getString("country.Code");
-                c.name = rset.getString("country.Name");
-                c.continent = rset.getString("country.Continent");
-                c.region = rset.getString("country.Region");
-                c.population = rset.getInt("country.Population");
-                c.capital = rset.getString("city.Name");*/
+                countryList.add(c);
+            }
+            return countryList;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
+    public ArrayList<Country> getCountryByRegion(String s_region) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect = "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.name FROM world.country left join world.city on country.Capital = city.ID WHERE country.Region = " + "'" + s_region + "'" + " order by country.Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            ArrayList<Country> countryList = new ArrayList<Country>();
+            while (rset.next()) {
+                Country c = new Country(rset.getString("country.Code"), rset.getString("country.Name"), rset.getString("country.Continent"),rset.getString("country.Region"),rset.getInt("country.Population"),rset.getString("city.Name"));
+                countryList.add(c);
+            }
+            return countryList;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
+    public ArrayList<Country> top_N_Continent(String s_continent, int numberOfContinents) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect = "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.name FROM world.country left join world.city on country.Capital = city.ID WHERE country.Continent = " + "'" + s_continent + "'" + " order by country.Population DESC LIMIT " + numberOfContinents;
+            // Execute SQL statement
+            System.out.println(strSelect);
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            ArrayList<Country> countryList = new ArrayList<Country>();
+            while (rset.next()) {
+                Country c = new Country(rset.getString("country.Code"), rset.getString("country.Name"), rset.getString("country.Continent"),rset.getString("country.Region"),rset.getInt("country.Population"),rset.getString("city.Name"));
                 countryList.add(c);
             }
             return countryList;
@@ -160,23 +188,22 @@ public class TestApp {
     public static void main(String[] args) {
         // Create new Application
         TestApp a = new TestApp();
-
+        Country c = new Country();
 
         // Connect to database
         a.connect();
-        ArrayList<Country> c = a.getCountryByPopulation();
-        for (Country country : c) {
-            String country_string =
-                    String.format("%-5s %-30s %-20s %-20s %-15s %-15s", country.code, country.name, country.continent, country.region, country.population, country.capital);
-            System.out.println(country_string);
-        }
+        ArrayList<Country> worldPop = a.getCountryByPopulation();
+        c.generateReport(worldPop);
 
-        ArrayList<Country> c1 = a.getCountryByContinent("Europe");
-        for (Country country : c1) {
-            String country_string =
-                    String.format("%-5s %-30s %-20s %-20s %-15s %-15s", country.code, country.name, country.continent, country.region, country.population, country.capital);
-            System.out.println(country_string);
-        }
+        ArrayList<Country> continentPop = a.getCountryByContinent("Europe");
+        c.generateReport(continentPop);
+
+        ArrayList<Country> regionPop = a.getCountryByRegion("Polynesia");
+        c.generateReport(regionPop);
+
+        ArrayList<Country> NCountries = a.top_N_Continent("Polynesia",3);
+        c.generateReport(NCountries);
+
         Country individualCountry = a.getCountry("Syria");
         //a.displayCountry(c);
         a.displayCountry(individualCountry);
